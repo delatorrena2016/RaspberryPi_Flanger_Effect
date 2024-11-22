c88564e9cbd7ef6f63f5fe417a3f8a0518a8d6fb
Alvaro de la Torre <37124283+delatorrena2016@users.noreply.github.com>
2024-11-22 14:14:03 -0600

    more comments

--- Flanger.py
+++ Flanger.py
@@ -1,27 +1,27 @@
 delay        = 0.01
 mod_width    = 0.003
-mod_freq     = 1
-fs           = 44.1E03 # sampling rate for audio CDs
-ratio        = mod_freq / fs
-M0           = np.floor(fs * delay)
-width        = np.floor(fs * mod_width)
-Nl           = 0.5 * fs
-g            = 1 # notch depth [0,1]
-phase        = 0
-i_n          = 0
+mod_freq     = 1                        # Rapidez del movimiento de las muescas (Hz)
+fs           = 44.1E03                  # Tasa de muestreo (Hz)
+ratio        = mod_freq / fs            # Taza de desplazamiento de muescas por cada muestra tomada
+M0           = np.floor(fs * delay)     # Longitud de retardo promedio o promedio densidad de muescas
+width        = np.floor(fs * mod_width) # Barrido o Oscilacion de retardo maximo
+g            = 1                        # Profundidad de las muescas [0,1]
+phase        = 0                        # Desface inicial nulo
+i_n          = 0                        # Inicialización de indice 
 # maximum delay
 # L = fs(delay + mod_width) + 2
 delay_length = M0 + width + 2 # f441 + f132.3 + 2 = 575
 delay_buffer = np.zeros(delay_length)
 
-def lfo(i=1):
+def lfo(i=1):  # Modulador de longitud de retrazo M
   global phase
-  # no. of notches
-  # if phase [offset = 0, 1/8], then [M0, fs(delay + md_width)]
+  # Mn o M(n) es el numero de picos en la respuesta de frecuencia,
+  # centrados al rededor de las frecuencias k(2pi/M), k=0,1,...,M-1.
+  # Extremos entre (M0-width, M0+width)
   Mn = M0 + width*np.sin(2*np.pi*phase)
-  # evolution of phase
-  phase  = phase + (i * ratio)
-  # closure over the phase, sin over one single period
+  # Se suma al valor anterior el "ratio" 0,2.27e-5,4.53e-5,...
+  phase  = phase + (i * ratio)  # Modulacion lenta caracteristica de flanging
+  # cerradura sobre un solo periodo fase toma [offset = 0, 1]
   if(phase > 1.0):
     phase = phase - 1.0
   return Mn
@@ -42,17 +42,17 @@ def push(sample):
   if i_n + 1 >= delay_length:
     i_n = i_n - delay_length
 
-#Funcion principal de la librería, modula la muestra a procesar desde el programa principal
-def flanger(x):
-# We are on digital domain so we're forced to clip the delay line
-#Al operar en tiempo discreto hay que redondear a entero las muestras de retardo
-  m = np.floor(lfo())
-  # how far in between [n-M, n-M+1] we want to find a value to compensate
-  # for the previous
-  eta = lfo() - m
-  # Le pasamos el retardo digital m
+def flanger(x):        # Mixer de señales dry y wet
+
+  m = np.floor(lfo())  # Longitud de retardo M(n) calculado por LFO
+  eta = lfo() - m      # retardo fraccional en las muestras, fraccion de indice entre n-M y n-M-1 
+  # Linea de retardo interpolada, ahora M(n) varia suavemente en valores no discretos
+  # x_eta = (1-eta)x(n-m) + eta x(n-m-1)
   x_eta = ((1 - eta) * delay_line(m)) + (eta * delay_line(m + 1))
 
   push(x)
-  return x + g * x_eta
+  # Mixer o Relacion de entrada-salida para efecto flanger sencillo,
+  # suma de la señal original (dry signal) x, y la señal retrasada (linea de retardo, aquí interpolada) x_eta,
+  # escalada por parametro de profundidad g 
+  return x + g * x_eta 
 
