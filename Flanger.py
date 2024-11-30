27776ad630519f0aab704c44535a26f20cb18193
Alvaro de la Torre <37124283+delatorrena2016@users.noreply.github.com>
2024-11-29 18:58:20 -0600

    Add files via upload
    Ahora si sirve

--- Flanger.py
+++ Flanger.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 delay        = 0.003                    # [0.003,0.01] retardo máximo de 3ms 
 mod_width    = 0.003                    # Cant. de profundidad
@@ -16,6 +17,8 @@ gain         = 0.8                      # Profundidad de las muescas o flangeo
 phase        = 0                        # Desface inicial nulo
 i_n          = 0                        # Inicialización de indice 
 # Retardo máximo y buffer de línea de retardo
+# El tamaño del buf. retardo es una unidad más grande que el,
+# valor máximo que M(n) puede tomar.
 delay_length = int(2 + M0 + width)
 delay_buffer = np.zeros(delay_length)
 #count        = 0
@@ -32,10 +35,9 @@ def lfo(i=1):  # Modulador de longitud de retardo M
   # cerradura sobre un solo periodo fase toma [offset = 0, 1]
   if(phase > 1.0):
     phase = phase - 1.0
-  return int(Mn)
+  return Mn
 
 def delay_line(i_m):
-  global i_n, delay_buffer
   # We force index from zero and above for inputs
   idx = i_n - i_m
   #print(i_n,i_m,idx)
@@ -45,28 +47,33 @@ def delay_line(i_m):
 
 def push(sample):
   global i_n, delay_buffer
-   # push sample and closure on/over superior limit
-  delay_buffer[i_n] = sample
-  i_n = i_n + 1
-  if i_n + 1 >= delay_length:
+  # Asignación punto a punto del buffer al buf. de retardo.
+  delay_buffer[i_n] = sample 
+  # Asignación de izquierda a derecha [0, len(delay_buffer)-1].
+  i_n = i_n + 1 
+  if i_n == delay_length:
     i_n = i_n - delay_length
+  # No asignamos punto al último espacio del buf. de retardo,
+  # Así como nunca lo leemos (por inicialización 0).
 
 def flanger(x):        # Mixer de señales dry y wet
   #global count
+  osc = lfo()
+  m = int(osc)  # Longitud de retardo M(n) calculado por LFO
+  # Manejo de error de truncamiento al acercarse al limite superior
+  if m == osc:   
+    m = m - 1
+  eta = osc - m      # Retardo fraccional entre muestras, indice de entre n-M y n-M-1 
   
-  m = np.floor(lfo())  # Longitud de retardo M(n) calculado por LFO
-  eta = lfo() - m      # retardo fraccional en las muestras, fraccion de indice entre n-M y n-M-1 
-
   #count = count + 1
   #if count > 1024:
   #  count = count - 1025
   
   # Linea de retardo interpolada, ahora M(n) varia suavemente en valores no discretos
-  # x_eta = (1-eta)x(n-m) + eta x(n-m-1)
+  # x_eta = x(n-m-eta) = (1-eta)*x(n-m) + eta*x(n-m-1)
   x_eta = ((1 - eta) * delay_line(m)) + (eta * delay_line(m + 1))
-  push(x)
-  # Mixer o Relacion de entrada-salida para efecto flanger sencillo,
-  # suma de la señal original (dry signal) x, y la señal retrasada (linea de retardo, aquí interpolada) x_eta,
-  # escalada por parametro de profundidad g 
-  return (feedback * x) + (gain * x_eta) 
+  push(x) # Nuevo punto en buf. de retardo
+
+  # suma de la señal original (dry signal) x, y la señal retrasada (linea de retardo, aquí interpolada) x_eta 
+  return (feedback * x) + (gain * x_eta) # Ecuación de diferencias de flangeo
 
