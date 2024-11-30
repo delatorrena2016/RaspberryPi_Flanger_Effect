085aa44b2c709150c91dc2b1065e31bdcd591186
richarnar22 <ricardo.arana4765@alumnos.udg.mx>
2024-11-29 19:14:33 -0600

    Update runmasterv3.py

--- runmasterv3.py
+++ runmasterv3.py
@@ -1,5 +1,7 @@
 
 
+
+
 import jack
 import numpy as np
 from Flanger import *
@@ -19,45 +21,78 @@ client.outports.register("output")
 
 
 
-# Configura el LED en el GPIO 14 para Flanger activo
-led = LED(14)
+# Configura el LED 
+ledf = LED(14)   # Flanger
+ledl = LED(2)   # Low Pass Filter
+ledh = LED(3)   # High Pass Filter
+ledb = LED(4)   # Band Pass Filter
 
 # Configura el botn en el GPIO 15
 buttonfla = Button(15)
 buttonlpf = Button(18)
+buttonhpf = Button(23)
+buttonbpf = Button(24)
 
 # Define lo que sucede cuando el botn es presionado
 def passthrough(x):
     return x
 
 f1 = passthrough
-f2 = f1
+f2 = passthrough
+f3 = passthrough
+f4 = passthrough
 
 def buttonfla_pressed():
     global f1
-    led.on()
+    ledf.on()
     f1 = flanger
 
 def buttonfla_released():
     global f1
-    led.off()
+    ledf.off()
     f1 = passthrough
 
 def buttonlpf_pressed():
     global f2
-    led.on()
+    ledl.on()
     f2 = lowpassf
 
 def buttonlpf_released():
     global f2
-    led.off()
+    ledl.off()
     f2 = passthrough
+
+def buttonhpf_pressed():
+    global f3
+    ledh.on()
+    f3 = highpassf
+
+def buttonhpf_released():
+    global f3
+    ledh.off()
+    f3 = passthrough
+
+def buttonbpf_pressed():
+    global f4
+    ledb.on()
+    f4 = bandpassf
+
+def buttonbpf_released():
+    global f4
+    ledb.off()
+    f4 = passthrough
+
+
 
 
 buttonfla.when_pressed = buttonfla_pressed
 buttonfla.when_released = buttonfla_released
 buttonlpf.when_pressed = buttonlpf_pressed
 buttonlpf.when_released = buttonlpf_released
+buttonhpf.when_pressed = buttonhpf_pressed
+buttonhpf.when_released = buttonhpf_released
+buttonbpf.when_pressed = buttonbpf_pressed
+buttonbpf.when_released = buttonbpf_released
 
 
 
@@ -69,7 +104,7 @@ write_index = 0  #ndice para escribir en el buffer
 # Funcin de callback para procesar el audio
 @client.set_process_callback
 def process(frames):
-    global write_index, buffer, f1, f2
+    global write_index, buffer, f1, f2, f3, f4 
 
     # Captura el audio de entrada
     in_data = client.inports[0].get_array()
@@ -91,7 +126,7 @@ def process(frames):
         out_data[i] = (buffer[i])/32767.0
        
         
-        out_data[i] = f2(f1(out_data[i]))
+        out_data[i] = f4(f3(f2(f1(out_data[i]))))
        
        
     out_data[:] = out_data
