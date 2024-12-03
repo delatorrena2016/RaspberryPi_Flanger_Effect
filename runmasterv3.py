46ed85852ff51f05c13c1333749ff8689b697ca3
richarnar22 <ricardo.arana4765@alumnos.udg.mx>
2024-12-02 20:30:12 -0600

    Comentarios actualizados

--- runmasterv3.py
+++ runmasterv3.py
@@ -21,69 +21,71 @@ client.outports.register("output")
 
 
 
-# Configura el LED 
+# Configura los leds de estado de los diferentes tratamientos digitales
 ledf = LED(14)   # Flanger
 ledl = LED(2)   # Low Pass Filter
 ledh = LED(3)   # High Pass Filter
 ledb = LED(4)   # Band Pass Filter
 
-# Configura el botn en el GPIO 15
-buttonfla = Button(15)
-buttonlpf = Button(18)
-buttonhpf = Button(23)
-buttonbpf = Button(24)
+# Configura el botones de interfaz para la activacion o desactivacion de los diferentes tratamientos
+buttonfla = Button(15) # Boton para Flanger
+buttonlpf = Button(18) # Boton para lowpassfilter
+buttonhpf = Button(23) # Boton para highpassfilter
+buttonbpf = Button(24) # Boton para bandpassfilter
 
-# Define lo que sucede cuando el botn es presionado
+# Funcion de passthrough para la senal sin tratamiento digital
 def passthrough(x):
     return x
-
+# Se inicializan las variables de los filtros aplicados a la senal de salida, todos se inicializan como passthrough
 f1 = passthrough
 f2 = passthrough
 f3 = passthrough
 f4 = passthrough
 
-def buttonfla_pressed():
+
+# Se definen todas la funciones conmutativas para los botones de activacion de los diferentes tratamientos
+def buttonfla_pressed():    # On Flanger
     global f1
     ledf.on()
     f1 = flanger
 
-def buttonfla_released():
+def buttonfla_released():    # Off Flanger
     global f1
     ledf.off()
     f1 = passthrough
 
-def buttonlpf_pressed():
+def buttonlpf_pressed():    # On LowPassfilter
     global f2
     ledl.on()
     f2 = lowpassf
 
-def buttonlpf_released():
+def buttonlpf_released():   # Off LowPassFilter
     global f2
     ledl.off()
     f2 = passthrough
 
-def buttonhpf_pressed():
+def buttonhpf_pressed():    # On HighPassfilter
     global f3
     ledh.on()
     f3 = highpassf
 
-def buttonhpf_released():
+def buttonhpf_released():    # Off highPassfilter
     global f3
     ledh.off()
     f3 = passthrough
 
-def buttonbpf_pressed():
+def buttonbpf_pressed():    # On BandPassfilter
     global f4
     ledb.on()
     f4 = bandpassf
 
-def buttonbpf_released():
+def buttonbpf_released():    # Off BandPassfilter
     global f4
     ledb.off()
     f4 = passthrough
 
 
-
+# Se asocian los botones con las funciones definidas
 
 buttonfla.when_pressed = buttonfla_pressed
 buttonfla.when_released = buttonfla_released
@@ -96,12 +98,12 @@ buttonbpf.when_released = buttonbpf_released
 
 
 
-# Tamo del buffer circular, por ejemplo, 4096 muestras
+# Tamono del buffer circular,  4096 muestras y definicion del formato, se inicializan las variables de la senal de entrada
 buffer_size = 4096
 buffer = np.zeros(buffer_size, dtype=np.int16)
 write_index = 0  #ndice para escribir en el buffer
 
-# Funcin de callback para procesar el audio
+# Funcion de callback para procesar el audio
 @client.set_process_callback
 def process(frames):
     global write_index, buffer, f1, f2, f3, f4 
@@ -111,10 +113,10 @@ def process(frames):
 
     # Usamos el buffer circular
     for i in range(frames):
-        # Escribir los datos en el buffer en la posicin correspondiente
+        # Escribir los datos en el buffer en la posicion correspondiente
         buffer[write_index] = np.int16(in_data[i]*32767)
 
-        # Avanzamos el ndice de escritura, y si llega al final, volvemos al principio
+        # Avanzamos el indice de escritura, y si llega al final, volvemos al principio
         write_index = (write_index + 1) % buffer_size
 
         # Enviar los datos a la salida (en este caso, directamente sin modificar)
@@ -126,7 +128,7 @@ def process(frames):
         out_data[i] = (buffer[i])/32767.0
        
         
-        out_data[i] = f4(f3(f2(f1(out_data[i]))))
+        out_data[i] = f4(f3(f2(f1(out_data[i])))) # Se aplican los 4 filtros a la senal de salida, de no tener filtros activos la senal = passthrough
        
        
     out_data[:] = out_data
